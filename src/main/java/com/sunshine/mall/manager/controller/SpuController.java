e960cb07cdd02b5dd9dd6d433a55e9f10e89a6f8
huxingnan <huxingnan@hotmail.com>
2017-08-09 22:11:20 +0800

    commit_code

--- src/main/java/com/sunshine/mall/manager/controller/SpuController.java
+++ src/main/java/com/sunshine/mall/manager/controller/SpuController.java
@@ -1,23 +1,16 @@
 package com.sunshine.mall.manager.controller;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
-import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 import com.sunshine.mall.manager.bean.T_Product;
 import com.sunshine.mall.manager.service.ProductService;
-import com.sunshine.mall.manager.util.MyStringUtil;
 import com.sunshine.mall.manager.util.MyUploadUtil;
 
 @Controller
@@ -34,13 +27,13 @@ public class SpuController {
 	
 	@RequestMapping("/saveProductInfoAndImage")
 	public String saveProductInfoAndImage(T_Product product ,@RequestParam("files") MultipartFile[] files){
-		List<String> filenamelist=new ArrayList<String>();
-		filenamelist =MyUploadUtil.saveFile(files);
+		List<String> filenamelist =MyUploadUtil.saveFile(files);
 		
-		productService.saveProduct(product);//主键返回 
+		productService.saveProduct(product,filenamelist);//主键返回 
 		
-		Integer product_id = product.getId();
-		productService.saveProductImageById(product_id, filenamelist);
+		//Integer product_id = product.getId();//获取生成的产品id
+		//productService.saveProductImageById(product_id, filenamelist);
+		
 		return "redirect:/spu/main.do";
 	}
 	
